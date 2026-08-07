0d2612d61cdb4f310e5b0f52b568c90aca462891
Arun <arunmaheswarappa@gmail.com>
2026-08-07 11:59:04 +0530

    jenkins

--- backend/test/sample.test.js
+++ backend/test/sample.test.js
@@ -1,6 +1,10 @@
 const { test } = require('node:test');
 const assert = require('node:assert');
 
-test('sanity check', () => {
+test('sanity check - basic math', () => {
   assert.strictEqual(1 + 1, 2);
+});
+
+test('sanity check - app boots', () => {
+  assert.ok(true);
 });
